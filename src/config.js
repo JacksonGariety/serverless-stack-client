cfdf033cf163313df0c46d89e5d1fad63dc819be
Coleman Gariety <garietyc@reed.edu>
2019-11-03 17:44:49 -0800

    React scaffolding, bootstrap, and authentication with AWS Amplify.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "note-bucket-coleman"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://k5hwd4msy8.execute-api.us-west-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_iddXII9PN",
+    APP_CLIENT_ID: "32chvt2ps1rasempo7v4nu02ct",
+    IDENTITY_POOL_ID: "us-west-2:0f7242aa-ca1a-43bb-897f-e67510e9bdec"
+  }
+};
